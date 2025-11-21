e2ad82ccf036d4abf4cb37aa8fb1d3e04430576d
JamesWebbTelescope <Viktorfrom@gmail.com>
2025-11-21 17:42:39 +0100

    Built app

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,11 +11,11 @@ dotenv.config({ path: path.resolve(__dirname, 'globals.env') });
 
 // https://vite.dev/config/
 export default defineConfig({  
-  define: {
+  /*define: {
     base: '/',
     'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
     'import.meta.env.VITE_VERSION': JSON.stringify(process.env.VITE_VERSION),
-  },
+  },*/
   plugins: [
     react({
       babel: {
