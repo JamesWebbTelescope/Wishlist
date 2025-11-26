ac683794cc6d9ad7565fc352a5552fc87a15b893
JamesWebbTelescope <Viktorfrom@gmail.com>
2025-11-26 17:23:44 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import "./index.css"
 import Home from "./pages/Home";
 import WishPage from "./pages/Wishes"
