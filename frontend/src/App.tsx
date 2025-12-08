fc03b8a8b2d22b99e6a74a14199519bc8a21c531
JamesWebbTelescope <Viktorfrom@gmail.com>
2025-12-08 16:05:11 +0100

    Removed basename

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -26,7 +26,7 @@ export default function App() {
 
       <WishPage/>
       <div>
-      <HashRouter basename="/">
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/frontend/src/pages/Wishes" element= {<WishPage/>}/>
